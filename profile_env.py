a493c135cac485d163319c6c940f9243ef88c296
Viraj Mehta <virajm@cs.cmu.edu>
2020-07-20 20:14:27 -0500

    profile env skeleton

---
+++ profile_env.py
@@ -0,0 +1,15 @@
+import gym
+import keras
+
+class ProfileEnv(gym.env):
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def seed(self, seed=None):
+        pass
+
+    def step(self, action):
+        return state, reward, done, {}
